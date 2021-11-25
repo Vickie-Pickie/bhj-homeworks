9b9cd8fd483730512a6079f8d839c8495fca9510
viktoriamastrenko <victoriamastrenko@gmail.com>
2021-11-25 12:05:22 -0600

    Добавлена возможность закрытия этого уже открытого меню

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -5,13 +5,18 @@ menuLinksArr.forEach(elem => {
 });
 
 function openMenu(e) {
+    let subMenu = e.target.nextElementSibling;
+    if (subMenu && subMenu.classList.contains("menu_active")) {
+        subMenu.classList.remove("menu_active");
+        return false;
+    }
+
     let menu = e.target.closest(".menu");
     let menuActive = menu.querySelectorAll(".menu_active");
     Array.from(menuActive).forEach( elem => {
         elem.classList.remove("menu_active");
     });
 
-    let subMenu = e.target.nextElementSibling;
     if (subMenu) {
         subMenu.classList.add("menu_active");
         return false;
